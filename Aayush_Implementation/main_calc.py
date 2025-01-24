bb51d6419454ea2e5f23d64fdd7cc3a9f21fa81f
Aashmun Gupta <aashmun.gupta@iitgn.ac.in>
2025-01-24 15:24:09 +0530

    Removed redundant files

--- Aayush_Implementation/main_calc.py
+++
@@ -1,8 +0,0 @@
-from parser import *
-from ast_class import *
-
-if __name__ == "__main__":
-    expr = "if {2 < 3} then { if {4>5} then {10 + 10} else {100+100} end } else {1 + 2 + 2} end"
-    ast = parse(expr)
-    result = e(ast)
-    print(result)
